5195d7ea11b91db19e553f4626ed13bb604e490a
vidya <vsagarj@gmail.com>
2012-11-17 14:14:04 -0800

    read the input word list into a 'dictionary' and split it into segments, one for each alphabet
    dictionary_search.rb
     - read input data file
     - split word list into segments - one for each letter
    
    test_dictionary_search.rb
     - ensure that the word list has been read into the dictionary
     - ensure that the dictionary is split into segments - one per alphabet

--- dictionary_search.rb
+++ dictionary_search.rb
@@ -1,7 +1,29 @@
+
+require 'pry'
+
 class DictionarySearch
+  attr_accessor :dict, :letter_segment
+
   def initialize(file_path)
+    @dict             = read_data file_path
+    @letter_segment   = get_letter_segments
+    #binding.pry
   end
 
   def word_pairs
+  end
+
+  #--- private
+  private
+  def read_data(filename)
+    File.readlines(filename).map { |ln| ln.chomp }
+  end
+
+  def get_letter_segments
+    ('a'..'z').inject({}) do |let_seg_hash, let|
+      let_seg_hash[let] = dict.select { |word| word.start_with? let }
+
+      let_seg_hash
+    end
   end
 end
--- test_dictionary_search.rb
+++ test_dictionary_search.rb
@@ -1,7 +1,25 @@
-class DictionarySearch
-  def initialize(file_path)
+require 'pry'
+
+require 'minitest/autorun'
+require './dictionary_search'
+
+class TestDictionarySearch < MiniTest::Unit::TestCase
+  attr_accessor :dict_search
+
+  def setup
+    @dict_search = DictionarySearch.new('wordsEn.txt')
   end
 
-  def word_pairs
+  def test_that_dictionary_is_available
+    dict = @dict_search.dict
+
+    refute_empty dict, '--- error: dictionary is not available'
+  end
+
+  def test_that_dictionary_is_split_into_segments
+    letter_segment_keys = dict_search.letter_segment.keys
+
+    assert_equal letter_segment_keys, ('a'..'z').to_a, '--- error: unexpected letter segment keys'
   end
 end
+

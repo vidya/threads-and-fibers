f1c4a28e2e4c9b28193e32a6e6f3e556b2a2076a
vidya <vsagarj@gmail.com>
2012-11-21 14:32:28 -0800

    Simplify
    fiber_dictionary_search.rb
     - simplify code

--- fiber_dictionary_search.rb
+++ fiber_dictionary_search.rb
@@ -1,32 +1,55 @@
 
 require 'pry'
+require 'fiber'
 
 require './lib_dictionary_search'
 
-class FiberWorker
-  include LibDictionarySearch
-
-  attr_accessor :fib, :let_list,:letter_segment
-
-  def initialize(let_list, letter_segment)
-    @let_list = let_list
-    @fib = Fiber.new {}
-    @letter_segment = letter_segment
-  end
-
-  def continue_work
-    #puts "89 let_list = #{let_list.inspect}"
-    let = @let_list.pop
-    @let_list = @let_list[0..-1]
-
-    puts "bu76 let = #{let}"
-    return nil if let.nil?
-
-    yield  select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
-  end
-end
+#class FiberWorker #< Fiber
+#  include LibDictionarySearch
+#
+#  attr_accessor :fib, :let_list,:letter_segment
+#
+#  def initialize(let_list, letter_segment)
+#    @let_list = let_list
+#    #@fib = Fiber.new {}
+#    @letter_segment = letter_segment
+#
+#    #binding.pry
+#    @fib = Fiber.new do
+#      return nil if @let_list.empty?
+#
+#      let, @let_list = @let_list.pop, @let_list[0..-1]
+#      puts "let = #{let}"
+#
+#      #yield
+#      yield select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+#    end
+#  end
+#
+#  def continue_work(&block)
+#    @fib ||= Fiber.new do
+#      return nil if @let_list.empty?
+#
+#      let, @let_list = @let_list.pop, @let_list[0..-1]
+#      puts "let = #{let}"
+#
+#      yield  select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+#    end
+#
+#    @fib.resume(&block)
+#  end
+#
+#  #def continue_work
+#  #  return nil if @let_list.empty?
+#  #
+#  #  let, @let_list = @let_list.pop, @let_list[0..-1]
+#  #
+#  #  yield  select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+#  #end
+#end
 
 class FiberDictionarySearch
+  include LibDictionarySearch
   attr_accessor :dict, :letter_segment, :word_count, :alphabet_list, :reversible_suffix_words
 
   def initialize(file_path)
@@ -43,14 +66,48 @@ class FiberDictionarySearch
 
   def word_pairs
     if reversible_suffix_words.empty?
-      fw1 = FiberWorker.new ('a'..'h').to_a, letter_segment
-      fw2 = FiberWorker.new ('i'..'r').to_a, letter_segment
-      fw3 = FiberWorker.new ('s'..'z').to_a, letter_segment
+
+      list1 = ('a'..'h').to_a
+      fw1 = Fiber.new do
+        while not list1.empty?
+          let, list1 = list1.pop, list1[0..-1]
+          puts "let = #{let}"
+
+          Fiber.yield select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+        end
+      end
+
+      list2 = ('i'..'r').to_a
+      fw2 = Fiber.new do
+        while not list2.empty?
+
+         let, list2 = list2.pop, list2[0..-1]
+          puts "let = #{let}"
+
+          Fiber.yield select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+        end
+
+      end
+
+      list3 = ('s'..'z').to_a
+      fw3 = Fiber.new do
+        while not list3.empty?
+
+          let, list3 = list3.pop, list3[0..-1]
+          puts "let = #{let}"
+
+          Fiber.yield select_reversible_suffix_words delete_tiny_words(@letter_segment[let])
+        end
+      end
 
       while true
-        rv1 = fw1.continue_work { |rv_list| append_rv_list rv_list }
-        rv2 = fw2.continue_work { |rv_list| append_rv_list rv_list }
-        rv3 = fw3.continue_work { |rv_list| append_rv_list rv_list }
+        rv1 = fw1.resume(rv1) if fw1.alive?
+        rv2 = fw2.resume(rv2) if fw2.alive?
+        rv3 = fw3.resume(rv3) if fw3.alive?
+
+        append_rv_list rv1 unless rv1.nil?
+        append_rv_list rv2 unless rv2.nil?
+        append_rv_list rv3 unless rv3.nil?
 
         break unless (rv1 || rv2 || rv3)
       end
